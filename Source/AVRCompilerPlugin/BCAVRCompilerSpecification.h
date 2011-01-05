6f70c9261897460ec1067adf136123408c2e5ffd
Jeremy Knope <jerome@buttered-cat.com>
2011-01-04 22:15:46 -0500

    started creating a proper compiler plugin

---
+++ Source/AVRCompilerPlugin/BCAVRCompilerSpecification.h
@@ -0,0 +1,16 @@
+//
+//  BCAVRCompilerSpecification.h
+//  AVRCompilerPlugin
+//
+//  Created by Jeremy Knope on 1/4/11.
+//  Copyright 2011 Buttered Cat Software. All rights reserved.
+//
+
+#import "XCPSpecifications.h"
+
+
+@interface BCAVRCompilerSpecification : XCCompilerSpecification {
+
+}
+
+@end
---
+++ Source/AVRCompilerPlugin/BCAVRCompilerSpecification.m
@@ -0,0 +1,14 @@
+//
+//  BCAVRCompilerSpecification.m
+//  AVRCompilerPlugin
+//
+//  Created by Jeremy Knope on 1/4/11.
+//  Copyright 2011 Buttered Cat Software. All rights reserved.
+//
+
+#import "BCAVRCompilerSpecification.h"
+
+
+@implementation BCAVRCompilerSpecification
+
+@end
