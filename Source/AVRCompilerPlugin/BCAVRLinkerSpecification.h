41e0ac3845578acda424eb3aab95f3e3d2192a79
Jeremy Knope <jerome@buttered-cat.com>
2011-01-09 19:34:23 -0500

    can't use .pde files until we do a preprocessor that renames them before avr-gcc gets it.  linker now works. arduino libraries probably won't work yet though

---
+++ Source/AVRCompilerPlugin/BCAVRLinkerSpecification.h
@@ -0,0 +1,17 @@
+//
+//  BCAVRLinkerSpecification.h
+//  AVRCompilerPlugin
+//
+//  Created by Jeremy Knope on 1/9/11.
+//  Copyright 2011 Ambrosia Software, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "XCPSpecifications.h"
+
+@interface BCAVRLinkerSpecification : XCLinkerSpecification {
+@private
+    
+}
+
+@end
